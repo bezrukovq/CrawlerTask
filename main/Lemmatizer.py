2f8ec5ac056dd8741f07e7aa341d579173c75254
tim_baton <tim_baton@mail.ru>
2021-03-23 18:04:35 +0300

    finish boolean search

--- main/Lemmatizer.py
+++ main/Lemmatizer.py
@@ -1,13 +1,10 @@
-# coding=utf-8
 import codecs
 import string
 import pymorphy2
 import re
-
 from bs4 import BeautifulSoup
 from nltk import word_tokenize
 from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
 
 # list of symbols to be removed
 MARKS = [',', '.', ':', '?', '«', '»', '-', '(', ')', '!', "“", "„", "–", '\'', "—", ';', "”", "...", "\'\'",
@@ -37,84 +34,67 @@ def parse_words(html_file):
     return text.split()
 
 
-# clear text
-def remove_unnecessary_symbols(words):
-    # clear text from the unnecessary symbols
-    text_without_marks = remove_marks(words)
-    text_without_stop_words = remove_stopwords(text_without_marks)
-    text_without_numbers = remove_non_alphabet_symbols(text_without_stop_words)
-    return text_without_numbers
-
-
 # remove marks
-def remove_marks(words):
-    # remove marks words
-    filtered_words = list(
-        filter(lambda word: (word not in string.punctuation) and (word not in MARKS), words))
+# remove stop words and make it to lowercase
+# remove non alphabetic strings
+# remove marks words
+# remove marks chars
+def remove_unnecessary_symbols(words):
+    stop_words = set(stopwords.words('russian'))
+    filtered_words = set()
+    for word in words:
+        if ((word not in string.punctuation) and
+                (word not in MARKS) and
+                (word not in stop_words) and
+                (word.isalpha())):
+            filtered_words.add(word)
 
     # remove marks symbols
     result = []
     for filtered_word in filtered_words:
-        result.append("".join(filter(lambda char: char not in MARKS, filtered_word)))
+        result.append("".join(filter(lambda char: char, filtered_word)))
     return result
-
-
-# remove stop words and make it to lowercase
-def remove_stopwords(words):
-    stop_words = set(stopwords.words('russian'))
-    filtered_sentences = [word.lower() for word in words if word not in stop_words]
-    return filtered_sentences
-
-
-# remove non alphabetic strings
-def remove_non_alphabet_symbols(words):
-    return filter(lambda word: word.isalpha(), words)
 
 
 # write our cleared text into the file
 def write_clear_words_into_file(text):
-    file = open(WORDS_TXT, "w", encoding="utf-8")
+    file = open(WORDS_TXT, "a", encoding="utf-8")
     for word in text:
         file.write(word + '\n')
+    file.close()
 
 
 # the mystery box.
-def lemmatize():
-    # read clear words
-    lst = open(WORDS_TXT, "r", encoding="utf-8")
-    words = lst.readlines()
-
+def lemmatizing_and_indexing(clear_words, file, index_dict):
     # dictionary with word-token elements
     lem_dict = {}
-    index_dict = {}
 
-    for word in words:
+    for word in clear_words:
         normal_form = get_normal_form(word.strip())
         if normal_form:
             # if the word doesn't exist, put it as a key
             if normal_form[0] not in lem_dict.keys():
                 lem_dict[normal_form[0]] = [word.strip()]
-                index_dict[normal_form] = [int(word[1])]
             # if the word already exist, put it's not normal form as a value
             else:
-                index_dict[normal_form].append(int(word[1]))
                 lem_dict[normal_form[0]].append(word.strip())
-    for key in index_dict.keys():
-        index_dict[key].sort()
+
+            # if the word doesn't exist in indexes, put it as a key
+            if normal_form[0] not in index_dict:
+                index_dict[normal_form[0]] = [file]
+                # if the word already exist, put it's not normal form as a value
+            else:
+                index_dict[normal_form[0]].append(file)
+
     # write our result into output.txt file
-    file = open(OUTPUT_TXT, "w", encoding="utf-8")
+    file = open(OUTPUT_TXT, "a", encoding="utf-8")
     for word, tokens in lem_dict.items():
         file.write("{word}:" + word)
         [file.write(" {token}:" + token) for token in set(tokens)]
         file.write("\n")
     file.close()
-    indexfile = open(INDEX_TXT, "w", encoding="utf-8")
-    for word, indexes in index_dict.items():
-        indexfile.write(f"{word}")
-        [indexfile.write(f" {index}") for index in set(indexes)]
-        indexfile.write("\n")
-    indexfile.close()
-    return(lem_dict, index_dict)
+
+    return index_dict
 
 
 # get the normal form of the word
@@ -123,50 +103,64 @@ def get_normal_form(word):
     analyzer = pymorphy2.MorphAnalyzer()
     normalized_words = []
     for token in tokens:
-        if token in string.punctuation:
-            continue
-        if token in MARKS:
-            continue
         normalized_words.append(analyzer.parse(token)[0].normal_form)
+
     return normalized_words
 
 
 def boolean_search(text, index_dict):
-
     pattern = r"[^\w]"
     text = re.sub(pattern, " ", text)
     words = text.split()
-    # TODO - words = filter_words(words)
 
     normal_words = []
 
     for word in words:
-        normal_words.append(get_normal_form(word))
+        form = get_normal_form(word)[0]
+        normal_words.append(form)
 
     page_numbers = []
     for word in normal_words:
-        if(word in index_dict.keys()):
-            page_numbers.extend(index_dict[word])
-    page_numbers.sort()
-    return page_numbers
+        if word in index_dict:
+            page_numbers.append(index_dict[word])
+
+    union = page_numbers[0]
+    for numbers in page_numbers:
+        union = union & numbers
+    return union
+
 
 if __name__ == '__main__':
-    # get list with files
+    # clear files
+    file = open(OUTPUT_TXT, "w", encoding="utf-8")
+    file.close()
+    file_words = open(WORDS_TXT, "w", encoding="utf-8")
+    file_words.close()
+
     indexes = open("pages/index.txt", "r")
-    arr_words = []
     indexes_lines = indexes.readlines()
     file_names = [line[: line.find(" ")] for line in indexes_lines]
+    index_dict = {}
 
-    # get all alphabetic words and write it into the file
+    # get all alphabetic words and write it into the file of tokens and indexes
     for file_name in file_names:
         file_name_txt = "%s.txt" % file_name
-        arr_words.extend(parse_words(file_name_txt))
-    clear_text = remove_unnecessary_symbols(arr_words)
-    write_clear_words_into_file(clear_text)
+        all_words = parse_words(file_name_txt)
+        clear_words = remove_unnecessary_symbols(all_words)
+        write_clear_words_into_file(clear_words)
+        index_dict = (lemmatizing_and_indexing(clear_words, file_name, index_dict))
 
-    # do the lemmatization
-    lem_dict, index_dict = lemmatize()
+    # write indexes into inverted_index.txt
+    index_file = open(INDEX_TXT, "w", encoding="utf-8")
+    index_dict = dict(sorted(index_dict.items()))
+    for word, indexes in index_dict.items():
+        index_dict[word] = set(indexes)
+        index_file.write(f"{word}")
+        [index_file.write(f" {index}") for index in indexes]
+        index_file.write("\n")
 
-    text = "захватывающая история"
+    index_file.close()
+    text = "свежий сайт"
     page_numbers = boolean_search(text, index_dict)
     print(page_numbers)
+#     the result will be {'26', '59', '5', '38', '77', '46', '30', '28', '58', '75', '40', '23', '1', '36', '78', '25', '95', '41', '70', '3', '35', '68', '105', '52', '20', '96', '2', '88', '18', '6', '17', '21', '50', '61', '104', '73', '33', '11', '60', '91', '4', '87', '62', '66', '71', '54', '12', '43', '47', '49', '53', '64', '103', '24', '8', '39', '80', '81', '22', '93', '32', '67', '85', '10', '76', '92', '9', '14', '89', '94', '51', '7', '74', '16', '99', '13', '69', '55', '72', '83', '42', '34', '44', '102', '86', '97', '63', '57', '19', '27', '90', '15', '29', '48', '37', '82', '79', '98', '65'}
